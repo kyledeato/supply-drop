845f1ca4829e8caebfd37790532a547cec917cf1
Hiago Garcia <garcia_hiago@hotmail.com>
2022-06-24 13:30:42 -0700

    Created the base folders, env file, mongoose, and jwt configs.

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require("cookie-parser");
+require('./configs/mongoose.configs');
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+require('./routes/forum.routes')(app);
+require("dotenv").config();
+
+const port = 8000;
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
