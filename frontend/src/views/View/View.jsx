15f03b07155ec27c59c4cf986ce541ff41bed941
Kyle Deato <kyledeato@gmail.com>
2022-06-25 11:10:20 -0700

    add google maps api

--- frontend/src/views/View/View.jsx
+++ frontend/src/views/View/View.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const View = () => {
+  const [map, setMap] = useState('')
+  const key = process.env.REACT_APP_SECRET_API_KEY
+  // need to change based on user input
+  const url = `https://maps.googleapis.com/maps/api/staticmap?center=Kyiv,Ukraine&zoom=14&size=400x400&key=${key}`
+
+  axios.get(url).then(response=> {
+    console.log(response)
+    setMap(response.data[0])
+    .catch(err=>console.log(err))
+  },[])
+
   return (
-    <div>View when click the articles try to make it a pop up</div>
+    <div>View when click the articles try to make it a pop up
+
+      <img src={url} alt="" />
+    </div>
   )
 }
 
