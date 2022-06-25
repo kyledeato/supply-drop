2b1784f7c014d3e6103049e27b6fd432accba053
lexCarey <lexcarey73@gmail.com>
2022-06-25 13:28:20 -0700

    NavBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Logout from '../Logout/Logout'
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div>COMPOMNENT---NavBar
-
-        
+    <div className='nav flex align-center'>
+      <div className='flex align-center'>
+        <h1>Supply Drop</h1>
+        <input type="text" placeholder='Search' />
+      </div>
+      <div className='flex align-center'>
+        <a className='no-border' href="/">Home</a>
+        <a href="/allposts">All Posts</a>
+        <a href="/create">Create a Post</a>
+        <a href="/account">Account</a>
+        <Logout />
+      </div>
     </div>
   )
 }
