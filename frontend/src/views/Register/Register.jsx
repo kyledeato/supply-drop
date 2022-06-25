03b6994b73834a2d9048e6d81e798f0c3884506b
Christiani Igot <c.bensi.igot@gmail.com>
2022-06-25 15:51:49 -0700

    implemented login/registration/edit user component

--- frontend/src/views/Register/Register.jsx
+++ frontend/src/views/Register/Register.jsx
@@ -1,23 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import UserRegister from "../../components/RegisterForm/UserRegister";
+import React from "react";
+import UserForm from "../../components/UserForm/UserForm";
 
 const Register = () => {
-    const[user, setUser] = useState()
-
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/user')
-            .then(res => {
-                setUser(res.data)
-            })
-    })
-
-  return (
-    <div>
-        <header><h1>Create User</h1></header>
-        <UserRegister user={user}/>
+    return (
+        <div>
+            <header>
+                <h1>Create User</h1>
+            </header>
+            <UserForm mode={"register"} />
         </div>
-  )
-}
+    );
+};
 
-export default Register
+export default Register;
