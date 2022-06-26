7a9d86b83306fe0e90268139fd138dc03b9f0588
Christiani Igot <c.bensi.igot@gmail.com>
2022-06-26 14:08:58 -0700

    Added modal popup for post view, create, edit

--- frontend/src/views/Home/Home.jsx
+++ frontend/src/views/Home/Home.jsx
@@ -1,49 +1,78 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom'
-import PostForm from '../../components/PostForm'
-import HomePosts from '../../components/HomePosts/HomePosts'
-import NavBar from '../../components/NavBar/NavBar'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
+import PostForm from '../../components/PostForm';
+import HomePosts from '../../components/HomePosts/HomePosts';
+import NavBar from '../../components/NavBar/NavBar';
 
 const Home = () => {
-    const [user, setUser] = useState()
+    const [user, setUser] = useState();
+    const [bigForm, setBigForm] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post("http://localhost:8000/api/user/logout", {}, { withCredentials: true })
-            .then(res => {
-                setUser(null)
-                navigate('/')
+        axios
+            .post(
+                'http://localhost:8000/api/user/logout',
+                {},
+                { withCredentials: true }
+            )
+            .then((res) => {
+                setUser(null);
+                navigate('/');
             })
-            .catch(err => console.log(err))
-    }
+            .catch((err) => console.log(err));
+    };
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/auth`, { withCredentials: true })
-            .then(res => {
-                setUser(res.data)
+        axios
+            .get(`http://localhost:8000/api/auth`, { withCredentials: true })
+            .then((res) => {
+                setUser(res.data);
             })
-            .catch(err => console.log(err))
-    }, [])
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <h1>HOMEPAGE</h1>
-            { user && <PostForm userID={user._id}/> }
+            {user && (
+                <a
+                    href="#newpost"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setBigForm(true);
+                    }}
+                >
+                    Create New Post
+                </a>
+            )}
+            {bigForm && (
+                <PostForm
+                    embiggenForm={(index, active) => {
+                        setBigForm(active);
+                    }}
+                    userID={user._id}
+                />
+            )}
             <HomePosts />
-            {user ? <div className="logged">
-                <p>
-                    Signed in as: {user.firstName} {user.lastName} {user._id}
-                </p>
-                <button onClick={handleSubmit}>Logout</button>
-            </div>
-            :
-            <Link to="/login">Login</Link>}
+            {user ? (
+                <div className="logged">
+                    <p>
+                        Signed in as: {user.firstName} {user.lastName}{' '}
+                        {user._id}
+                    </p>
+                    <button onClick={handleSubmit}>Logout</button>
+                </div>
+            ) : (
+                <Link to="/login">Login</Link>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
