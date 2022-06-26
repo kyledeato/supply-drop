112f96b6dfff2bab128347bffba205f3061c4387
Christiani Igot <c.bensi.igot@gmail.com>
2022-06-26 09:17:00 -0700

    added functionality to view all posts

--- frontend/src/components/HomePosts/HomePosts.jsx
+++ frontend/src/components/HomePosts/HomePosts.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const HomePosts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/post/")
+      .then((res) => {
+        setPosts(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div>HomePosts RECENT AND TOP</div>
-  )
-}
+    <div>
+      {posts.map((post) => (
+        <div key={post._id}>
+          <div>
+            {post.image ? (
+              <img
+                src={"http://localhost:8000/img/" + post.image}
+                alt={post.title}
+              />
+            ) : (
+              <span>no image</span>
+            )}
+          </div>
+          <div>
+            <div>
+              <span>{post.title}</span> :: <span>{post.location}</span>
+            </div>
+            <div>{post.description}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default HomePosts
+export default HomePosts;
