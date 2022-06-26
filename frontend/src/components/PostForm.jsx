0ee059e12fa07fdd46fe08ae6687d21a588a83dc
Aaron <aaroncourt09@gmail.com>
2022-06-25 20:26:20 -0700

    bug fix of PostForm component

--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -4,26 +4,23 @@ import {useParams, Link, useNavigate} from "react-router-dom";
 
 const PostForm = (props) => {
   const {postID, userID} = useParams();
-  const [postInfo, setPostInfo] = useState();
-  const [postTitle, setPostTitle] = useState();
-  const [postDesc, setPostDesc] = useState();
-  const [postType, setPostType] = useState();
-  const [postLocation, setPostLocation] = useState();
+  const [postTitle, setPostTitle] = useState('');
+  const [postDesc, setPostDesc] = useState('');
+  const [postType, setPostType] = useState('');
+  const [postLocation, setPostLocation] = useState('');
   const [postImage, setPostImage] = useState('test')
-  const [errors, setErrors] = useState();
+  const [errors, setErrors] = useState('');
 
   async function getPostInfo(){
     try{
       const response = await axios.get(`http://locatlhost:8000/api/post/${postID}`, {withCredentials: true});
-      setPostInfo({
-        'postID' : response.data._id,
-        'title' : response.data.title,
-        'description' : response.data.description,
-        'postType' : response.data.postType,
-        'location' : response.data.location,
-        'image' : response.data.image,
-        'postedBy' : response.data.postedBy
-      })
+      postID = response.data._id;
+      setPostTitle(response.data.title);
+      setPostDesc(response.data.description);
+      setPostType(response.data.postType);
+      setPostLocation(response.data.location)
+      setPostImage(response.data.image)
+      var postedBy = response.data.postedBy
     } 
     
     catch(err) {
@@ -72,14 +69,14 @@ const PostForm = (props) => {
         {/* image container */}
         <div>
           <label className='' htmlFor='postPicture'>Add a photo</label>
-          <input type={'file'} accept='.png, .jpg, .jpeg' name='postPicture'  />
+          <input type={'file'} accept='.png, .jpg, .jpeg' name='postPicture'  value={postImage}/>
         </div>
         {/* end post container */}
 
         {/* post information container */}
         <div>
           <label htmlFor="title">Post Title:</label>
-          <input type={'text'} name='title' className='' onChange={(e) => setPostTitle(e.target.value)} value={postID.title} />
+          <input type={'text'} name='title' className='' onChange={(e) => setPostTitle(e.target.value)} value={postTitle} />
 
           <label className=''>What type of post is it?</label>
           {/* radio button conditionals */}
@@ -115,10 +112,10 @@ const PostForm = (props) => {
           }
           
           <label className='' htmlFor='desc'>Description of item(s)</label>
-          <textarea className='' name='' draggable='false' rows={'16'} cols={'50'} value={postID.description} onChange={(e) => setPostDesc(e.target.value)}/>
+          <textarea className='' name='' draggable='false' rows={'16'} cols={'50'} value={postDesc} onChange={(e) => setPostDesc(e.target.value)}/>
 
           <label className='' htmlFor='location'>Location of item</label>
-          <input type={'text'} className='' name='location' value={postID.location} onChange={(e) => setPostLocation(e.target.value)}/>
+          <input type={'text'} className='' name='location' value={postLocation} onChange={(e) => setPostLocation(e.target.value)}/>
 
           <button type='submit' className='' >Submit</button>
         </div>
