cde56b2f11a6f8e9d03b236cfb044b4ad009b9c7
Christiani Igot <c.bensi.igot@gmail.com>
2022-06-26 14:08:03 -0700

    added formdata parser and formatting

--- backend/routes/supply_drop.routes.js
+++ backend/routes/supply_drop.routes.js
@@ -1,24 +1,28 @@
-const UserController = require("../controllers/users.controller");
-const PostController = require("../controllers/posts.controller");
-const { authenticate } = require("../configs/jwt.config");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const UserController = require('../controllers/users.controller');
+const PostController = require('../controllers/posts.controller');
+const { authenticate } = require('../configs/jwt.config');
+const multer = require('multer');
+const formdataParser = multer({ dest: 'uploads/' });
 
 module.exports = function (app) {
-  //User routes
-  app.post("/api/user/register", UserController.register);
-  app.post("/api/user/login", UserController.login);
-  app.post("/api/user/logout", UserController.logout);
-  app.get("/api/user", UserController.getAllUsers);
-  app.get("/api/user/:id", UserController.getUser);
-  app.put("/api/user/:id", UserController.updateUser);
-  app.delete("/api/user/:id", UserController.deleteUser);
-  app.get("/api/auth", authenticate, UserController.getLoggedUser);
+    //User routes
+    app.post('/api/user/register', UserController.register);
+    app.post('/api/user/login', UserController.login);
+    app.post('/api/user/logout', UserController.logout);
+    app.get('/api/user', UserController.getAllUsers);
+    app.get('/api/user/:id', UserController.getUser);
+    app.put('/api/user/:id', UserController.updateUser);
+    app.delete('/api/user/:id', UserController.deleteUser);
+    app.get('/api/auth', authenticate, UserController.getLoggedUser);
 
-  //Post Routes
-  app.post("/api/post/new", upload.single("photo"), PostController.createPost);
-  app.get("/api/post/", PostController.getAllPosts);
-  app.get("/api/post/:id", PostController.getPost);
-  app.put("/api/post/:id", PostController.updatePost);
-  app.delete("/api/post/:id", PostController.deletePost);
+    //Post Routes
+    app.post(
+        '/api/post/new',
+        formdataParser.single('photo'),
+        PostController.createPost
+    );
+    app.get('/api/post/', PostController.getAllPosts);
+    app.get('/api/post/:id', PostController.getPost);
+    app.put('/api/post/:id', formdataParser.none(), PostController.updatePost);
+    app.delete('/api/post/:id', PostController.deletePost);
 };
