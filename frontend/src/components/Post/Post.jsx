a5fd24f11ea88d69dc20d20576a75b6fd7d47012
Kyle Deato <kyledeato@gmail.com>
2022-06-27 12:13:31 -0700

    styling for pop up view a post

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './post.css';
+import locationLogo from './location.png';
+import xLogo from './x.png';
 
 function Post({
     postType,
@@ -16,19 +18,21 @@ function Post({
         <div className="post-container">
             <div className="post">
                 <div>
-                    <span>{title}</span> :: <span>{location}</span>{' '}
-                    <button
-                        onClick={() => {
+                    <div className='post-head-container'>
+                        <h5 className='title'>{title}</h5>
+                        <img src={xLogo} alt="" srcset="" onClick={() => {
                             embiggenPost(index, false, 'post');
-                        }}
-                    >
-                        [X]
-                    </button>
+                        }} className='x'/>
+                    </div>
+                    <img src={locationLogo} alt="" srcset=""className="locationImage"/><span className='location'>{location}</span>
+
+                        <p className='description'>{description}</p>
                 </div>
                 <div>
-                    <div>
+                    <div className='image'>
                         {image ? (
                             <img
+                                
                                 src={'http://localhost:8000/img/' + image}
                                 alt={title}
                             />
@@ -36,20 +40,33 @@ function Post({
                             <span>no image</span>
                         )}
                     </div>
-                    <iframe
-                        title="googlemap"
-                        width="300"
-                        height="300"
-                        loading="lazy"
-                        allowFullScreen
-                        referrerPolicy="no-referrer-when-downgrade"
-                        src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDUbRiOy26TfHguvbRi-9XsLa_oRNvQ_fY&q=${location}`}
-                    ></iframe>
-                    <p>{description}</p>
+                    <div className='bottom'>
+                        <form action="" className="email">
+                            <h1 className='title'>Send a Message</h1>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name"  required/>
+                            <label htmlFor="name" className='padding'>Email</label>
+                            <input type="email" name="email"  required/>
+                            <label htmlFor="message" className='padding'>Your Message</label>
+                            <textarea name="message" id="" cols="30" rows="4"></textarea>
+                        <div className='padding'>
+                            <button type="submit">Send</button>
+                        </div>
+                        </form>
+                        <iframe
+                            title="googlemap"
+                            width="300"
+                            height="300"
+                            loading="lazy"
+                            allowFullScreen
+                            referrerPolicy="no-referrer-when-downgrade"
+                            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDUbRiOy26TfHguvbRi-9XsLa_oRNvQ_fY&q=${location}`}
+                        ></iframe>
+                    </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default Post;
